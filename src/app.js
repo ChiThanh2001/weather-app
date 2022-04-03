50d6a180c74f89e145fc16f0388b1c87b3d5d3bd
chithanh2001 <satthumaulanh2001@gmail.com>
2022-04-03 11:30:58 +0700

    change title

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'index page',
+        title: 'Weather app',
         name: 'Lương Chí Thành'
     })
 })
